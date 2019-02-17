12fe8914bfcaaa372a63bf897a8c50cffe433781
Thomas Hansen <ctrhansen@yahoo.com>
2019-02-17 02:39:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,7 @@ def display_board
   puts row[0] = ["   ","   |   ","   ","   |   ","   "]
   puts row[1] = ["-----------"]
   puts row[2] = ["   ","   |   ","   ","   |   ","   "]
-  puts row[3] = []
+  puts row[3] = ["-----------"]
+  puts row[4] = ["   ","   |   ","   ","   |   ","   "]
   
 end
